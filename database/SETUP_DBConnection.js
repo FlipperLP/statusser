ca2de7ec1bd417374a004676432284ba778aeb3f
Phil <philip.schaefer0@gmail.com>
2020-04-29 00:18:38 +0200

    added db migration check

--- database/SETUP_DBConnection.js
+++ database/SETUP_DBConnection.js
@@ -4,6 +4,31 @@ const testToken = '../config/config.json';
 
 const config = require('../config/main.json');
 
+async function checkForMigrations(db) {
+  await console.log('[DB] Checking DB stucture');
+  const fs = require('fs');
+  const migrations = fs.readdirSync('./migrations');
+  const [checkDBMigration] = await db.query('SHOW TABLES LIKE \'SequelizeMeta\'', { type: Sequelize.QueryTypes.SELECT });
+  if (!checkDBMigration) {
+    await console.log('[DB] Missing tables! Creating...');
+    await db.queryInterface.createTable('SequelizeMeta', {
+      name: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+      },
+    });
+    await migrations.forEach(async (name) => {
+      const migration = require(`../migrations/${name}`);
+      migration.up(db.queryInterface, Sequelize);
+      await db.query('INSERT INTO `SequelizeMeta` VALUES(:name)', { type: Sequelize.QueryTypes.INSERT, replacements: { name } });
+    });
+    await console.log('[DB] Created!');
+  }
+  await console.log('[DB] Done!');
+}
+
 console.log('[DB] Connecting...');
 let database;
 let user;
@@ -25,6 +50,8 @@ const sequelize = new Sequelize(
   database, user, password, { host, dialect: 'mysql', logging: config.inDev },
 );
 console.log('[DB] Connected!');
+
+checkForMigrations(sequelize);
 
 module.exports = sequelize;
 global.sequelize = sequelize;

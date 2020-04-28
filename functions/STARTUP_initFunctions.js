b203969d5c09846c09a6cd65a1c8225796023df4
Phil <philip.schaefer0@gmail.com>
2020-04-28 23:23:01 +0200

    recoded functions-listing module

--- functions/STARTUP_initFunctions.js
+++ functions/STARTUP_initFunctions.js
@@ -1,28 +1,34 @@
-module.exports.run = async (client, fs, config) => {
-  const functionsFolder = config.setup.moduleFolders.functionsFolder;
+const fs = require('fs');
+
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+
+const config = require('../config/main.json');
+
+/* eslint-disable no-param-reassign */
+module.exports.run = async () => {
   // read directory with functions
-  fs.readdir(`./${functionsFolder}`, (err, files) => {
-    // error if fails
-    if (err) console.error(err);
+  const names = await readdir('./functions').catch((err) => console.error(err));
+  const functions = {};
+  // removal of '.js' in the end of the file
+  const jsfiles = names.filter((f) => f.split('.').pop() === 'js');
 
-    // removal of '.js' in the end of the file
-    const jsfiles = files.filter((f) => f.split('.').pop() === 'js');
+  // check if functions are there
+  if (jsfiles.length <= 0) return console.log(`[${module.exports.help.name}] No function(s) to load!`);
 
-    // check if functions are there
-    if (jsfiles.length <= 0) return console.log(`[${module.exports.help.name}] No function(s) to load!`);
+  if (config.inDev) console.log(`[${module.exports.help.name}] Loading ${jsfiles.length} function(s)...`);
 
-    if (config.env.get('inDev')) console.log(`[${module.exports.help.name}] Loading ${jsfiles.length} function(s)...`);
-
-    // adding all functions
-    jsfiles.forEach((f, i) => {
-      const probs = require(`../${functionsFolder}/${f}`);
-      if (config.env.get('inDev')) console.log(`[${module.exports.help.name}]     ${i + 1}) Loaded: ${f}!`);
-      // adding function to collection
-      client.functions.set(probs.help.name, probs);
-    });
-
-    console.log(`[${module.exports.help.name}] Loaded ${jsfiles.length} function(s)!`);
+  // adding all functions
+  jsfiles.forEach((f, i) => {
+    const probs = require(`../functions/${f}`);
+    if (config.inDev) console.log(`[${module.exports.help.name}]     ${i + 1}) Loaded: ${f}!`);
+    // adding function to collection
+    functions[probs.help.name] = probs;
   });
+
+  console.log(`[${module.exports.help.name}] Loaded ${jsfiles.length} function(s)!`);
+  return functions;
 };
 
 module.exports.help = {

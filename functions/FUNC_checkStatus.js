fba7689d8ecf32a94c5b8030a66476fe47c8a819
Phil <philip.schaefer0@gmail.com>
2020-04-28 23:21:05 +0200

    cleanup and varialbe implementation

--- functions/FUNC_checkStatus.js
+++ functions/FUNC_checkStatus.js
@@ -2,39 +2,51 @@ const puppeteer = require('puppeteer');
 
 const config = require('../config/main.json');
 
-// "domain": ".xn--allestrungen-9ib.de",
+// gets config Endpoint
+function getEndpoint(endpointCode) {
+  return config.downdetector.endpoints[endpointCode];
+}
 
+// closes page safely
+async function closePage(browser, page) {
+  await page.close();
+  await browser.close();
+}
+
+// requests website with requered service
 async function getPage(endpoint, service) {
   const browser = await puppeteer.launch({ headless: config.env.get('inDev') });
   const page = await browser.newPage();
   await page.setCookie(config.downdetector.consentCookie);
-  await page.goto('https://downdetector.pk/status/ea/');
-  return page;
+  await page.goto(`${endpoint}${service}/`);
+  return [browser, page];
 }
 
-module.exports.run = async () => {
-  const page = await getPage();
-
+module.exports.run = async (endpointCode, service) => {
+  // get endpoint adress from config and check if exists
+  const endpointAdress = await getEndpoint(endpointCode);
+  if (!endpointAdress) return 'noEndpoint';
+  const [browser, page] = await getPage(endpointAdress, service);
+  // DISABLED: not in use
   // await page.pdf({ path: './testing/withoutconsent.pdf', format: 'A4' });
 
-  const [output] = await page.$x('//*[@id="company"]/div[1]/div/div[1]');
-  // const text = await output.getProperty('innerText');
-
+  // check if page is a stauts page
   const problemButton = await page.evaluate(() => document.querySelector('a.btn.btn-danger.mt-2.px-4.py-3.shadow.mb-2'));
   if (!problemButton) {
-    await page.close();
-    await browser.close();
-    return null;
+    await closePage(browser, page);
+    return 'noServiceByName';
   }
-  const statusDanger = await page.evaluate(() => document.querySelector('.color-danger'));
-  const statusWarning = await page.evaluate(() => document.querySelector('.color-warning'));
 
-  const text = await output.getProperty('');
-  const down = await text.jsonValue();
-  console.log(down);
+  // get status by color
+  // const statusDanger = await page.evaluate(() => document.querySelector('.color-danger'));
+  // const statusWarning = await page.evaluate(() => document.querySelector('.color-warning'));
 
-  await page.close();
-  await browser.close();
+  // prepare return value
+  let finalStatus = 200;
+  if (await page.evaluate(() => document.querySelector('.color-danger'))) finalStatus = 'down';
+  if (await page.evaluate(() => document.querySelector('.color-warning'))) finalStatus = 'warn';
+  await closePage(browser, page);
+  return finalStatus;
 };
 
 module.exports.help = {

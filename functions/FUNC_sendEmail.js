e1e086168bd45eca3e3eeeba519e1d092cb9c44b
Phil <philip.schaefer0@gmail.com>
2020-04-28 23:21:53 +0200

    populated with code

--- functions/FUNC_sendEmail.js
+++ functions/FUNC_sendEmail.js
@@ -1,7 +1,29 @@
 const nodemailer = require('nodemailer');
 
-module.exports.run = async () => {
-  message.reply('del');
+module.exports.run = async (serviceName, status, email, config) => {
+  const testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: config.hostAdress,
+    port: config.port,
+    secure: config.SSLRequired,
+    auth: {
+      user: config.username,
+      pass: config.password,
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: `'Fred Foo 👻' <${config.username}>`,
+    to: email,
+    subject: `${serviceName} is ${status}`,
+    text: `The Service ${serviceName} went into a ${status} state!`,
+    html: `<b>The Service ${serviceName} went into a ${status} state!</b>`,
+  });
+
+  console.log(`Message sent: ${info.messageId}`);
 };
 
 module.exports.help = {

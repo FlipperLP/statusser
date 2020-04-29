2f12b6947b3022c1e5d29c8579e849a91b086601
Phil <philip.schaefer0@gmail.com>
2020-04-29 12:27:52 +0200

    added code comentery

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 (async () => {
+  // prepare env vars
   await require('./functions/STARTUP_envPrep').run();
+  // get functions
   const functions = await require('./functions/STARTUP_initFunctions').run();
 
-  // create conenction to DB
+  // create conenction to DB and check DB stucture
   require('./database/SETUP_DBConnection');
 
   await functions.TCKR_intervall.run(functions, 'vodafone');

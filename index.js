77fff57209976b0272a8d6d4863b8342eca4174c
Phil <philip.schaefer0@gmail.com>
2020-04-29 19:28:08 +0200

    moved DB code up

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 (async () => {
   // prepare env vars
   await require('./functions/STARTUP_envPrep').run();
+  // create conenction to DB and check DB stucture
+  await require('./database/SETUP_DBConnection');
   // get functions
   const functions = await require('./functions/STARTUP_initFunctions').run();
 
-  // create conenction to DB and check DB stucture
-  require('./database/SETUP_DBConnection');
-
-  await functions.TCKR_intervall.run(functions);
+  await functions.TCKR_downdetector.run(functions);
 })();

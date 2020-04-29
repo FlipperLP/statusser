411781748141db5c78643afc8d0b9837049a4ed9
Phil <philip.schaefer0@gmail.com>
2020-04-30 00:44:31 +0200

    updated error handler

--- functions/FUNC_errHandler.js
+++ functions/FUNC_errHandler.js
@@ -2,12 +2,12 @@ const Service = require('../database/models/service');
 
 const config = require('../config/main.json');
 
-module.exports.run = async (err, functions) => {
+module.exports.run = async (err) => {
   console.error(`[${module.exports.help.name}] There has been a runtime error: ${err}`);
   console.error(`[${module.exports.help.name}] Sending error to monitoring...`);
   const text = `Oh no! Statusser has run into an issue! \n\n ${err}`;
   const html = `<b>${text}</b>`;
-  await functions.FUNC_sendEmail.run(html, text, 'ERROR', [config.monitoringEmail]);
+  await require('./FUNC_sendEmail').run(html, text, 'ERROR', [config.monitoringEmail]);
   console.error(`[${module.exports.help.name}] Email sent!`);
 };
 

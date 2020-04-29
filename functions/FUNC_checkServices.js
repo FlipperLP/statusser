4d4c0750395bde4ebd21f15bee47497c3ff1e5e2
Phil <philip.schaefer0@gmail.com>
2020-04-29 19:28:22 +0200

    inital init

---
+++ functions/FUNC_checkServices.js
@@ -0,0 +1,11 @@
+const Service = require('../database/models/service');
+
+const config = require('../config/main.json');
+
+module.exports.run = async (functions) => {
+
+};
+
+module.exports.help = {
+  name: 'FUNC_checkServices',
+};
